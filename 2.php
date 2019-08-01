753890327e467fd94d1d316a10cee4d0de8910b9
gibransebagaisaturajp <gibransebagai2@gmail.com>
2019-08-01 17:00:50 +0700

    menambahkan soal 2

---
+++ 2.php
@@ -0,0 +1,17 @@
+<?php
+    $tama = 1;
+    $toni = 0.5;
+    $tita = 14;
+    $tito = 34;
+
+    echo "umur Tama = $tama<br>";
+    echo "umur Toni = $toni<br>";
+    echo "umur toni+tini = $tito<br>";
+    echo "$toni+$tama-$tita = $tito<br>"
+    $x = $tama + $toni;
+    $y = $tita - $tito;
+    $z = $x/$y;
+    echo "$x = $tita + $tito<br>";
+    echo "$x = $y<br>";
+    
+?>
